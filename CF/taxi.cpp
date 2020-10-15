188b5202adc5535cb60329349c3b25dd751fa432
saurav-07 <msm18b046@iiitdm.ac.in>
2020-10-15 17:38:43 +0530

    created DSA branch

---
+++ CF/taxi.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    long long n, taxi=0;
+    cin>>n;
+    int s[n];
+    for(int i=0; i<n; i++)
+        cin>>s[i];
+    
+    for(int i=0; i<n; i++){
+        for(int j=i; j<n; j++){
+            
+        }
+    }
+}
