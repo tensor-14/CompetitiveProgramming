c1bb2b1758e16443f4f53048a50e670a8e14d878
saurav-07 <msm18b046@iiitdm.ac.in>
2020-11-04 11:41:07 +0530

    Expression finished, dragons created

--- CF/caps-lock.cpp
+++ CF/caps-lock.cpp
@@ -3,13 +3,26 @@ using namespace std;
 
 int main(){
     string s;
+    bool flag;
     cin>>s;
 
-    for(int i=0; i<s.length(), i++){
+    for(int i=1; i<s.length(); i++){
         if(isupper(s[i]))
+            flag = true;
+        else{
+            flag = false;
+            break;
+        }
     }
-    transform(s.begin(), s.end(), s.begin(), ::tolower);
-    s[0] = toupper(s[0]);
+
+    if(flag and isupper(s[0])){
+        transform(s.begin(), s.end(), s.begin(), ::tolower);
+    }
+    else if(flag and islower(s[0])){
+        transform(s.begin(), s.end(), s.begin(), ::tolower);
+        s[0] = toupper(s[0]);
+    }
+    
     cout<<s;
 
     return 0;
---
+++ CF/dragons.cpp
@@ -0,0 +1,12 @@
+#include<algorithm>
+#include<vector>
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int s, n;
+    cin>>s>>n;
+
+    vector<int> ;
+    return 0;
+}
