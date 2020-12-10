d444711260e24bac2dd0ebd8407cb3a381a3697b
tensor-14 <msm18b046@iiitdm.ac.in>
2020-12-10 12:57:51 +0530

    Added test.cpp

---
+++ test-learn.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int x;
+    cin>>x;
+
+    for(int k=31; k>=0;k--){
+        if(x & (1<<k)) cout<<"1";
+        else cout<<"0";
+    }
+
+    cout<<"\n"<<(1<<x)<<endl;
+}
