bf99de99fa9d336343faafdb299eb7a80b612e46
tensor-14 <msm18b046@iiitdm.ac.in>
2021-03-21 22:35:10 +0530

    Atcoder and STL

---
+++ Atcoder/196/C.cpp
@@ -0,0 +1,87 @@
+#pragma GCC optimize("Ofast")
+#pragma GCC optimize("unroll-loops")
+#include<bits/stdc++.h>
+using namespace std;
+#define si(x) scanf("%d",&x)
+#define sl(x) scanf("%lld",&x)
+#define ss(s) scanf("%s",s)
+#define pi(x) printf("%d\n",x)
+#define pl(x) printf("%lld\n",x)
+#define ps(s) printf("%s\n",s)
+#define deb(x) cout << #x << "=" << x << endl
+#define deb2(x, y) cout << #x << "=" << x << "," << #y << "=" << y << endl
+#define PI 3.1415926535897932384626
+#define ll long long int
+#define endl "\n"
+
+mt19937_64 rang(chrono::high_resolution_clock::now().time_since_epoch().count());
+int rng(int lim) {
+	uniform_int_distribution<int> uid(0,lim-1);
+	return uid(rang);
+}
+
+ll mpow(ll base, ll exp); 
+
+const int mod = 1'000'000'007;
+const int N = 3e5, M = N;
+
+vector<int> g[N];
+int a[N];
+
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+string cnvt(ll num){
+	string s;
+	while(num){
+		s.push_back((char)num%10);
+		num/=10;
+	}
+	return s;
+}
+
+void solve(){
+	ll i, j, n, m, x, cnt=0;
+	cin>>n;
+
+	for(i=1; i<=n; i++){
+		if(((int)log10(i))%2==0){
+			string s=cnvt(i);
+
+			if(s.substr(0, s.size()/2) == s.substr(s.size()/2, s.size()/2))
+				cnt++;
+		}
+	}
+	cout<<cnt<<"\n";
+}
+
+int main(){
+	ios_base::sync_with_stdio(false), cin.tie(NULL), cout.tie(NULL);
+	srand(chrono::high_resolution_clock::now().time_since_epoch().count());
+
+	/*
+	#ifndef ONLINE_JUDGE 
+		freopen("input.txt", "r", stdin); 
+		freopen("output.txt", "w", stdout); 
+	#endif 
+	*/
+
+	int t = 1;
+//	cin >> t;
+	while(t--){
+		solve();
+	}
+
+	return 0;
+}
+
+ll mpow(ll base, ll exp) {
+	base %= mod;
+	ll result = 1;
+	while (exp > 0) {
+		if (exp & 1) result = ((ll)result * base) % mod;
+		base = ((ll)base * base) % mod;
+		exp >>= 1;
+	}
+	return result;
+}
+
